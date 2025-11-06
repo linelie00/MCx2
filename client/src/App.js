1ed7679c3751185e9ca6924fa4c99f9ee4353887
linelie00 <san00@cu.ac.kr>
2025-11-06 16:41:46 +0900

    [Feat]: 라우트 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import './Styles/App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './Pages/Home';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }

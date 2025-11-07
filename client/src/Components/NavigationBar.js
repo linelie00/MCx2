ac831c0a917ad2acd877c7def8c49fef3e3f90e3
linelie00 <san00@cu.ac.kr>
2025-11-07 11:40:06 +0900

    [Feat]: 스크롤 제거, 배경 추가

--- client/src/Components/NavigationBar.js
+++ client/src/Components/NavigationBar.js
@@ -3,7 +3,6 @@ import { Link, useNavigate } from 'react-router-dom';
 import '../Styles/Components.css';
 
 function NavigationBar() {
-  const navigate = useNavigate();
 
   return (
     <div className="bar">
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import '../Styles/App.css';
 
 const Home = () => {
     return (
         <>
-            <h1>Home Page</h1>
+            <div className="title">
+                <h1>MIHEARTI</h1>
+                <h2>Migeldoran Eternos Ozan Colddew</h2>
+                <h2>Matiam Crohi</h2>
+            </div>
+            <section className="bg-100w"></section>
+            <section className="section-385B44">
+            </section>
         </>
     );
 }

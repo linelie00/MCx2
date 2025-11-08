35ac524af7920100c90dc311acecf8241d9bd457
linelie00 <san00@cu.ac.kr>
2025-11-08 13:48:43 +0900

    [Feat: sticky 작동되게 수정, 배경 위치 수정]

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -4,11 +4,14 @@ import '../Styles/Home.css';
 const Home = () => {
     return (
         <>
-            <div className="title">
+            <div className="stickydiv">
                 <h1>MIHEARTI</h1>
+            </div>
+            <div className="title">
                 <h2>Migeldoran Eternos Ozan Colddew</h2>
                 <h2>Matiam Crohi</h2>
             </div>
+
             <section className="bg-100w"></section>
             <section className="section-385B44">
             </section>

da47f296a6e908703330a0dcb0de69a1826a9016
linelie00 <san00@cu.ac.kr>
2025-11-09 00:16:32 +0900

    [Feat]:캐릭터 세부 창일 때 nav 색상 변경되도록 수정

--- client/src/Components/NavigationBar.js
+++ client/src/Components/NavigationBar.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import { Link, NavLink, useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import '../Styles/Components.css';
 
 function NavigationBar() {
   const { pathname } = useLocation();
-  const showExtra = pathname !== "/";
+  const isMigel  = pathname === '/character/migel';
+  const isMatiam = pathname === '/character/matiam';
+
+  const barClass = [
+    'bar',
+    isMigel  && 'bar--migel',
+    isMatiam && 'bar--matiam',
+  ].filter(Boolean).join(' ');
 
   return (
-    <div className="bar">
-        <div className="nav-extra">
-            {showExtra && (
-                <Link to="">MIHEARTI</Link>
-            )}
-        </div>
-        <div className="nav-button">
-            <Link to="world">World</Link>
-            <Link to="character">Character</Link>
-            <Link to="story">Story</Link>
-            <Link to="image">Gallery</Link>
-            <Link to="playlist">Playlist</Link>
-        </div>
+    <div className={barClass}>
+      <div className="nav-extra">
+        {pathname !== '/' && <Link to="/">MIHEARTI</Link>}
+      </div>
+      <div className="nav-button">
+        <Link to="/world">World</Link>
+        <Link to="/character">Character</Link>
+        <Link to="/story">Story</Link>
+        <Link to="/image">Gallery</Link>
+        <Link to="/playlist">Playlist</Link>
+      </div>
     </div>
   );
 }

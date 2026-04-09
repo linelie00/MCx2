f778327938b81fa943ad8f0bcabf7bf28ff023d4
linelie00 <san00@cu.ac.kr>
2026-04-10 02:50:48 +0900

    [Feat]:반응형 변경

--- client/src/Components/NavigationBar.js
+++ client/src/Components/NavigationBar.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import '../Styles/Components.css';
 
 function NavigationBar() {
   const { pathname } = useLocation();
+  const [menuOpen, setMenuOpen] = useState(false);
+
   const isMigel  = pathname === '/character/migel';
   const isMatiam = pathname === '/character/matiam';
 
   const barClass = [
     'bar',
-    isMigel  && 'bar--migel',
+    isMigel && 'bar--migel',
     isMatiam && 'bar--matiam',
   ].filter(Boolean).join(' ');
 
@@ -18,12 +20,24 @@ function NavigationBar() {
       <div className="nav-extra">
         {pathname !== '/' && <Link to="/">MIHEARTI</Link>}
       </div>
-      <div className="nav-button">
-        <Link to="/world">World</Link>
-        <Link to="/character">Character</Link>
-        <Link to="/story">Story</Link>
-        <Link to="/image">Gallery</Link>
-        <Link to="/playlist">Playlist</Link>
+
+      {/* 햄버거 버튼 */}
+      <div
+        className={`hamburger ${menuOpen ? 'active' : ''}`}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      {/* 메뉴 */}
+      <div className={`nav-button ${menuOpen ? 'open' : ''}`}>
+        <Link to="/world" onClick={() => setMenuOpen(false)}>World</Link>
+        <Link to="/character" onClick={() => setMenuOpen(false)}>Character</Link>
+        <Link to="/story" onClick={() => setMenuOpen(false)}>Story</Link>
+        <Link to="/image" onClick={() => setMenuOpen(false)}>Gallery</Link>
+        <Link to="/playlist" onClick={() => setMenuOpen(false)}>Playlist</Link>
       </div>
     </div>
   );
